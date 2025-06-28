968cdf051f47a9eadb1415a555ef8143e1b58bb1
Jeison Rojas <j_esteban96@hotmail.com>
2025-06-28 11:45:34 -0500

    Documentation done with swagger to POST auth/register

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -5,6 +5,65 @@ import { RegisterUserSchema } from './auth.validation'
 
 const authRouter = Router()
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Registro de un nuevo usuario
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - lastname
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: Jeison
+ *               lastname:
+ *                 type: string
+ *                 example: Rojas
+ *               email:
+ *                 type: string
+ *                 example: jeison@example.com
+ *               password:
+ *                 type: string
+ *                 example: Pass123*
+ *     responses:
+ *       201:
+ *         description: Usuario creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 body:
+ *                   type: object
+ *                   properties:
+ *                     userId:
+ *                       type: number
+ *                       example: 1
+ *                     name:
+ *                       type: string
+ *                       example: Jeison
+ *                     lastname:
+ *                       type: string
+ *                       example: Rojas
+ *                     email:
+ *                       type: string
+ *                       example: jeison@example.com
+ *       400:
+ *         description: El usuario ya existe o hay errores de validación
+ */
 authRouter.post(
   '/register',
   validateRequest(RegisterUserSchema),
