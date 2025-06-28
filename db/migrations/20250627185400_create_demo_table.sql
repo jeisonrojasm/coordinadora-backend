d5ca70ce8607077270388d2a5623d6788d885a54
Jeison Rojas <j_esteban96@hotmail.com>
2025-06-27 19:18:56 -0500

    Migration process done

---
+++ db/migrations/20250627185400_create_demo_table.sql
@@ -0,0 +1,11 @@
+-- migrate:up
+CREATE TABLE IF NOT EXISTS users (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS users;
