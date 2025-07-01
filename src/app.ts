63df26a1e630e93a37a2d3701fb455bf847b125e
Jeison Rojas <j_esteban96@hotmail.com>
2025-07-01 00:35:53 -0500

    Status getAll service done

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import quoteRouter from './modules/quote/quote.routes'
 import shipmentRouter from './modules/shipment/shipment.routes'
 import { ApiException } from './utils/exceptions/ApiException'
 import cityRouter from './modules/city/city.routes'
+import statusRouter from './modules/status/status.routes'
 
 dotenv.config()
 
@@ -40,6 +41,7 @@ app.use('/auth', authRouter)
 app.use('/quote', quoteRouter)
 app.use('/shipment', shipmentRouter)
 app.use('/city', cityRouter)
+app.use('/status', statusRouter)
 
 // Middleware de errores global
 app.use(errorHandler)
